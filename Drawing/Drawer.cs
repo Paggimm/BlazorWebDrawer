f5f3c73ae2e73f82d3cc63af3c17e44be7abf096
paggimm <bernhardrobl@gmx.de>
2022-08-07 16:30:52 +0200

    refactored drawing call into separate class

---
+++ Drawing/Drawer.cs
@@ -0,0 +1,28 @@
+using Blazor.Extensions.Canvas.Canvas2D;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace DefaultNamespace;
+
+/// Class to actually draw on a given canvas
+/// drawing-properties are configured in DrawerConfig
+public class Drawer
+{
+    public bool DrawingEnabled { get; set; }
+    private readonly DrawerConfig _drawerConfig = new();
+    private readonly Canvas2DContext _canvasContext;
+
+    public Drawer( Canvas2DContext canvasContext)
+    {
+        _canvasContext = canvasContext;
+    }
+    
+    /// makes the actual draw on the canvas
+    public async void Draw(MouseEventArgs mouseEvent)
+    {
+        if (DrawingEnabled)
+        {
+            await _canvasContext.SetFillStyleAsync(_drawerConfig.Color);
+            await _canvasContext.FillRectAsync(mouseEvent.OffsetX - (_drawerConfig.BrushSize/2d), mouseEvent.OffsetY - (_drawerConfig.BrushSize/2d), _drawerConfig.BrushSize, _drawerConfig.BrushSize);
+        }
+    }
+}
---
+++ Models/DrawerConfig.cs
@@ -0,0 +1,10 @@
+namespace DefaultNamespace;
+
+/// <summary>
+/// class to configure meta-drawing stuff like color and brush size 
+/// </summary>
+public class DrawerConfig
+{
+    public string Color { get; set; } = "#357FFF";
+    public int BrushSize { get; set; } = 100;
+}
